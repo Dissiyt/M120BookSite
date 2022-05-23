f81ea516dcb1daffb50e42f48c204d72003be325
Dissiyt <dissi.yt221@gmail.com>
2022-05-23 18:15:14 +0200

    p1.0

--- index.php
+++ index.php
@@ -12,6 +12,9 @@
 
 
     $conn = new mysqli($host, $username, $password, $database);
+    $sql = "SELECT title FROM buecher WHERE verkauft = 0";
+    $result = $conn->query($sql);
+    $listen = mysqli_fetch_all($result, MYSQLI_ASSOC);
     ?>
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
@@ -19,7 +22,32 @@
     <title>Home</title>
 </head>
 <body>
-    <h1>Home</h1>
+    <nav class="navbar bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">
+                <img src="smart.png" alt="" width="30" height="30"class="d-inline-block align-text-top">
+                Smart Capital
+            </a>
+
+            <a class="nav-link" aria-current="page" href="#">Home</a>
+            <a class="nav-link" href="#">Link</a>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+    <h1 class="display-1 text-center">Home</h1>
     <p1>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p1>
+    <?php foreach($listen as $liste){ ?>
+       <!-- <div style="margin-left:20px">
+            <label for="Title">Title</label>
+            <br>
+            <span name="Title" class="align-baseline"><?php //echo htmlspecialchars($liste['title']);?> </span>
+
+
+        </div> !-->
+
+    <?php } ?>
 </body>
 </html>
