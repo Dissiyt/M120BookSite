f9c9bc89521e000197b3b5f42ff57cde12e2d908
Dissiyt <dissi.yt221@gmail.com>
2022-06-06 21:10:38 +0200

    p1.0

---
+++ dbconnect.php
@@ -0,0 +1,12 @@
+<?php
+
+    $host = 'localhost';
+    $database = 'books';
+    $username = 'root';
+    $password = 'root';
+    $liste = '';
+
+
+
+    $conn = new mysqli($host, $username, $password, $database);
+?>
--- register.php
+++ register.php
@@ -12,14 +12,11 @@
 
 
     $conn = new mysqli($host, $username, $password, $database);
-    $sql = "SELECT title FROM buecher WHERE verkauft = 0";
-    $result = $conn->query($sql);
-    $listen = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
     ?>
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="stylesheet.css">
     <title>Home</title>
 </head>
 <body>
@@ -54,17 +51,8 @@
         </div>
     </div>
 </nav>
-    <h1 class="display-1 text-center">Home</h1>
-    <p1>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p1>
-    <!-- <?php foreach($listen as $liste){ ?>
-       <div style="margin-left:20px">
-            <label for="Title">Title</label>
-            <br>
-            <span name="Title" class="align-baseline"><?php //echo htmlspecialchars($liste['title']);?> </span>
+<h1 class="display-1 text-center">Register</h1>
+<p1>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p1>
 
-
-        </div>
-
-    <?php } ?> !-->
 </body>
 </html>
